6932d288222f3487bf2bcefa9aba699a72332303
Buszewicz <filip.buszewicz@gmail.com>
2025-03-06 16:09:38 +0100

    init supabase

--- stiknes_mobile/lib/main.dart
+++ stiknes_mobile/lib/main.dart
@@ -1,23 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://arpxrljtyjqsvdeiylts.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFycHhybGp0eWpxc3ZkZWl5bHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk4ODYyOTgsImV4cCI6MjA1NTQ2MjI5OH0.14oKr6hHY_7y7wrokdEGGkzQkKVAjZvXv8SQwBAwqcU',
+  );
+
+  runApp(MyApp());
 }
 
+final supabase = Supabase.instance.client;
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Stikness',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color.fromARGB(255, 0, 0, 255),
-        ),
-        useMaterial3: true,
-      ),
+      title: 'Stiknes',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const LoginScreen(),
     );
   }
